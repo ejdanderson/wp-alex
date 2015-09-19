5781a2c949e4a1886f604c86b87ae7df91174482
Evan Anderson <kdfiction@gmail.com>
2015-09-19 16:54:00 -0600

    Alex now loaded in via tinyMCE plugin, remove submodule

--- wp-alex.php
+++ wp-alex.php
@@ -9,13 +9,6 @@ Author URI: http://ejdanderson.com
 License: GPLv2 or later
 */
 
-function wpalex_enqueue_scripts() {
-	$base_url = trailingslashit( plugins_url( basename( dirname( __FILE__ ) ) ) );
-	wp_enqueue_script('alexjs', $base_url . '/lib/alex/alex.js' , array() );
-	//wp_enqueue_script('wpalex', $base_url . 'wp-alex.js' , array( 'alexjs' ) );
-}
-add_action( 'admin_enqueue_scripts', 'wpalex_enqueue_scripts' );
-
 function wpalex_load_tinymce_plugin( $plugins ) {
 	$plugins['alextinymce'] =  plugins_url( '/lib/tinymce-alex/tinymce-alex.js', __FILE__ ) ;
 	return $plugins;
